8d7dd861590f09dd0d83f04bc5f7847b7c8d6c70
Start2Run <mihaivs@yahoo.com>
2020-11-23 20:08:21 -0500

    Add BDD specflow .feature file for the REST API functionality

---
+++ tests/SpecFlow_BDD/Steps/QueryRestApiEndpointSteps.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using TechTalk.SpecFlow;
+
+namespace SpecFlow_BDD.Steps
+{
+    [Binding]
+    public class QueryRestApiEndpointSteps
+    {
+        [Given(@"a valid endpoint address is configured in the application configuration file")]
+        public void GivenAValidEndpointAddressIsConfiguredInTheApplicationConfigurationFile()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [When(@"the request is sent to a Rest Api endpoint")]
+        public void WhenTheRequestIsSentToARestApiEndpoint()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"the returned body data should have the format JSON data model")]
+        public void ThenTheReturnedBodyDataShouldHaveTheFormatJSONDataModel()
+        {
+            ScenarioContext.Current.Pending();
+        }
+    }
+}
