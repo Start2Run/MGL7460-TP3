9aae7e7f984e14f3b92b95023c39c8375068282f
Start2Run <MihaiOlaru@protonmail.ch>
2020-11-26 19:31:36 -0500

    Create clean persistence data BDD tests

---
+++ tests/SpecFlow_BDD/Steps/CleanPersistanceDataSteps.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using TechTalk.SpecFlow;
+
+namespace SpecFlow_BDD.Steps
+{
+    [Binding]
+    public class CleanPersistanceDataSteps
+    {
+        [Given(@"a database name is configured in the application configuration file")]
+        public void GivenADatabaseNameIsConfiguredInTheApplicationConfigurationFile()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [When(@"a query is created to celan the DB")]
+        public void WhenAQueryIsCreatedToCelanTheDB()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"all the existing peristed temperature data should be deleted from the DB")]
+        public void ThenAllTheExistingPeristedTemperatureDataShouldBeDeletedFromTheDB()
+        {
+            ScenarioContext.Current.Pending();
+        }
+    }
+}
