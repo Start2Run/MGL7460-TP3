f5220b3b9b60119d501f9fb688992d1dc46a73cd
RajOrang <60149919+RajOrang@users.noreply.github.com>
2020-12-09 18:32:04 -0500

    Create build

---
+++ build/AutoBuild/ExecuteCmd.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AutoBuild
+{
+    public class ExecuteCmd
+    {
+        public void ExecuteCommandSync(object command)
+        {
+            try
+            {
+                System.Diagnostics.ProcessStartInfo procStartInfo = new System.Diagnostics.ProcessStartInfo("cmd", "/c " + command);
+                procStartInfo.RedirectStandardOutput = true;
+                procStartInfo.UseShellExecute = false;
+                procStartInfo.CreateNoWindow = true;
+                System.Diagnostics.Process proc = new System.Diagnostics.Process();
+                proc.StartInfo = procStartInfo;
+                proc.Start();
+
+                // Get the output into a string
+                string result = proc.StandardOutput.ReadToEnd();
+
+                // Display the command output.
+                Console.WriteLine(result);
+            }
+            catch (Exception objException)
+            {
+                // Log the exception
+            }
+        }
+
+        public void ExecuteCommandAsync(string command)
+        {
+            try
+            {
+                //Asynchronously start the Thread to process the Execute command request.
+                Thread objThread = new Thread(new ParameterizedThreadStart(ExecuteCommandSync));
+                //Make the thread as background thread.
+                objThread.IsBackground = true;
+                //Set the Priority of the thread.
+                objThread.Priority = ThreadPriority.AboveNormal;
+                //Start the thread.
+                objThread.Start(command);
+            }
+            catch (ThreadStartException objException)
+            {
+                // Log the exception
+            }
+            catch (ThreadAbortException objException)
+            {
+                // Log the exception
+            }
+            catch (Exception objException)
+            {
+                // Log the exception
+            }
+        }
+    }
+}
